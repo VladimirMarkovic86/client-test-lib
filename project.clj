d08001b021408ff4c20dada84333cb19c4bbd59d
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:43:57 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject org.vladimir/client-test-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/client-test-lib "0.1.0"
   :description "Simple client test library"
-  :url "http://gitlab:1610/VladimirMarkovic86/client-test-lib"
+  :url "https://github.com/VladimirMarkovic86/client-test-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.vladimir/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/cljs"])
 
