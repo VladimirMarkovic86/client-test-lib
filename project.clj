0d6912b00436e7c648078a4aa7192bd886a138e3
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-12 15:49:31 +0200

    Task 88: Implemented droid-server project, it is as library to be used with AndroidServer android java project.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/client-test-lib "0.2.13"
+(defproject org.clojars.vladimirmarkovic86/client-test-lib "0.2.14"
   :description "Simple client test library"
   :url "https://github.com/VladimirMarkovic86/client-test-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.10"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.16"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.6"]
                  ]
